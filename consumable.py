0b01e1c43a58b79a957116ac9c0eba648d708fe2
Zogalicious <zogalicious@protonmail.ch>
2024-04-11 14:19:14 +0100

    start getting consumable collisions with snake setup

--- consumable.py
+++ consumable.py
@@ -9,23 +9,24 @@ class Consumable:
     bounds = None
     blocks_in_x = None;
     blocks_in_y = None;
-    effect = 'increase multiplier'
+    effect = None
 
     def __init__(self, block_size, bounds):
         self.block_size = block_size
         self.bounds = bounds
-        self.blocks_in_x = (self.bounds[0])/self.block_size;
-        self.blocks_in_y = (self.bounds[1])/self.block_size;
+        self.blocks_in_x = int(self.bounds[0]/self.block_size);
+        self.blocks_in_y = int(self.bounds[1]/self.block_size);
 
     def draw(self, game, window):
         game.draw.rect(window, self.color, (self.x, self.y, self.block_size, self.block_size))
 
     def spawn(self):
-        effect = random.choice([True, False])
-        self.x = random.randint(150, self.blocks_in_x - 1) * self.block_size
-        self.y = random.randint(150, self.blocks_in_y - 1) * self.block_size
+        effect = 'increase multiplier' if random.choice([True, False]) else 'decrease multiplier'
+        self.x = random.randint(0, self.blocks_in_x - 1) * self.block_size
+        self.y = random.randint(0, self.blocks_in_y - 1) * self.block_size
 
     def is_collided_with(self, sprite):
+        rect_consumable = pygame.Rect(left=self.x, top=self.y, width=self.blocks_in_x, height=self.blocks_in_y)
         return self.rect.colliderect(sprite.rect)
 
     def scroll(self):
